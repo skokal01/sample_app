86444510d16284714825639e663982dd016af9ef
Santhosh Kokala <skokal01@gmail.com>
2012-02-13 16:42:02 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -11,6 +13,7 @@ SampleApp::Application.routes.draw do
   root :to => 'pages#home'
 
 
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
